ec944a8c98bb94f43b769c8e8547dc178c44d816
Thiago Marinho <tgmarinho@gmail.com>
2020-04-11 13:59:29 -0400

    adapt code to test

--- src/__tests__/projects.spec.js
+++ src/__tests__/projects.spec.js
@@ -9,7 +9,7 @@ describe("Projects", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        techs: ["Node", "Express", "TypeScript"],
       });
 
     expect(isUuid(response.body.id)).toBe(true);
@@ -18,7 +18,7 @@ describe("Projects", () => {
       url: "https://github.com/Rocketseat/umbriel",
       title: "Umbriel",
       techs: ["Node", "Express", "TypeScript"],
-      likes: 0
+      likes: 0,
     });
   });
 
@@ -28,7 +28,7 @@ describe("Projects", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        techs: ["Node", "Express", "TypeScript"],
       });
 
     const response = await request(app).get("/repositories");
@@ -40,8 +40,8 @@ describe("Projects", () => {
           url: "https://github.com/Rocketseat/umbriel",
           title: "Umbriel",
           techs: ["Node", "Express", "TypeScript"],
-          likes: 0
-        }
+          likes: 0,
+        },
       ])
     );
   });
@@ -52,7 +52,7 @@ describe("Projects", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        techs: ["Node", "Express", "TypeScript"],
       });
 
     const response = await request(app)
@@ -60,7 +60,7 @@ describe("Projects", () => {
       .send({
         url: "https://github.com/Rocketseat/unform",
         title: "Unform",
-        techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+        techs: ["React", "ReactNative", "TypeScript", "ContextApi"],
       });
 
     expect(isUuid(response.body.id)).toBe(true);
@@ -68,14 +68,12 @@ describe("Projects", () => {
     expect(response.body).toMatchObject({
       url: "https://github.com/Rocketseat/unform",
       title: "Unform",
-      techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+      techs: ["React", "ReactNative", "TypeScript", "ContextApi"],
     });
   });
 
   it("should not be able to update a repository that does not exist", async () => {
-    await request(app)
-      .put(`/repositories/123`)
-      .expect(400);
+    await request(app).put(`/repositories/123`).expect(400);
   });
 
   it("should not be able to update repository likes manually", async () => {
@@ -84,18 +82,16 @@ describe("Projects", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["React", "ReactNative", "TypeScript", "ContextApi"]
+        techs: ["React", "ReactNative", "TypeScript", "ContextApi"],
       });
 
     const response = await request(app)
       .put(`/repositories/${repository.body.id}`)
       .send({
-        likes: 15
+        likes: 15,
       });
 
-    expect(response.body).toMatchObject({
-      likes: 0
-    });
+    expect(response.statusCode).toBe(400);
   });
 
   it("should be able to delete the repository", async () => {
@@ -104,23 +100,19 @@ describe("Projects", () => {
       .send({
         url: "https://github.com/Rocketseat/umbriel",
         title: "Umbriel",
-        techs: ["Node", "Express", "TypeScript"]
+        techs: ["Node", "Express", "TypeScript"],
       });
 
-    await request(app)
-      .delete(`/repositories/${response.body.id}`)
-      .expect(204);
+    await request(app).delete(`/repositories/${response.body.id}`).expect(204);
 
     const repositories = await request(app).get("/repositories");
 
-    const repository = repositories.body.find(r => r.id === response.body.id);
+    const repository = repositories.body.find((r) => r.id === response.body.id);
 
     expect(repository).toBe(undefined);
   });
 
   it("should not be able to delete a repository that does not exist", async () => {
-    await request(app)
-      .delete(`/repositories/123`)
-      .expect(400);
+    await request(app).delete(`/repositories/123`).expect(400);
   });
 });
